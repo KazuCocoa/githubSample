00630c340c8390251e6dcc3963f4a82413f240d2
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2014-07-03 02:25:54 +0900

    enable commit

--- web.rb
+++ web.rb
@@ -3,24 +3,22 @@ require 'octokit'
 
 REPO = 'KazuCocoa/tagTestRepository'
 
-
 Octokit.api_endpoint = 'https://api.github.com'
 Octokit.web_endpoint = 'https://github.com'
-
-@client = Octokit::Client.new access_token: '9d75246f8907b18fa22d879f80bd15be19c7f75d'
 
 get '/' do
 
   #client = Octokit::Client.new :login  => 'defunkt', :password => 'c0d3b4ssssss!'
+  client = Octokit::Client.new access_token: '9d75246f8907b18fa22d879f80bd15be19c7f75d'
 
-  user = @client.user
+  user = client.user
   user.login
 
-  issues = @client.list_issues(REPO)
+  issues = client.list_issues(REPO)
 
   #"The number of issue is #{issues[0][:number]}"
   #"The title of issue is #{issues[0][:title]}"
 
-  @client.add_comment(REPO, issues[0][:number], "ついかコメント2！！")
+  client.add_comment(REPO, issues[0][:number], "ついかコメント3！！")
   "finish!!"
 end
