ad17592ce3e69a39eccd9b16e21b3c458a5feb52
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2014-07-03 07:39:13 +0900

    update web.rb

--- web.rb
+++ web.rb
@@ -30,22 +30,5 @@ get '/log' do
 end
 
 post '/hook_sample' do
-  if params['X-GitHub-Event'] == 'pull_request'
-
-    #client = Octokit::Client.new :login  => 'defunkt', :password => 'c0d3b4ssssss!'
-    client = Octokit::Client.new access_token: '9d75246f8907b18fa22d879f80bd15be19c7f75d'
-
-    user = client.user
-    user.login
-
-    issues = client.list_issues(REPO)
-
-    #"The number of issue is #{issues[0][:number]}"
-    #"The title of issue is #{issues[0][:title]}"
-
-    client.add_comment(REPO, issues[0][:number], params[:pull_request])
-    client.add_comment(REPO, issues[0][:number], 'finish!!')
-  else
-    "else"
-  end
+  "${params['X-GitHub-Event']}"
 end
