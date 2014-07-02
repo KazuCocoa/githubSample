ae4f79a390823569dd7ab253405687004edac184
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2014-07-03 01:53:43 +0900

    fix endpoint

--- web.rb
+++ web.rb
@@ -4,9 +4,10 @@ require 'octokit'
 REPO = 'KazuCocoa/tagTestRepository'
 
 
+Octokit.api_endpoint = 'https://api.github.com'
+Octokit.web_endpoint = 'https://github.com'
+
 get '/' do
-  Octokit.api_endpoint = 'https://api.github'
-  Octokit.web_endpoint = 'https://github'
 
   #client = Octokit::Client.new :login  => 'defunkt', :password => 'c0d3b4ssssss!'
   client = Octokit::Client.new access_token: '9d75246f8907b18fa22d879f80bd15be19c7f75d'
