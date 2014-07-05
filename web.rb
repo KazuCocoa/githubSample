e04de30649c2f698cc81b3a9ae7956b005d5225b
kazuaki MATSUO <fly.49.89.over@gmail.com>
2014-07-05 13:08:36 +0900

    update webapp

--- web.rb
+++ web.rb
@@ -50,7 +50,7 @@ post '/hook_sample' do
         client.add_comment(REPO, client.list_issues(REPO).first['number'], "PRが閉じたよ！")
         data = 'close PR'
       else
-        data = request.body
+        data = req_body['action']
       end
 
     when 'issues'
